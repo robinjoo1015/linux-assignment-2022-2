98556d98c71faa78e0699d85572c6f2fbbec3033
robinjoo1015 <robinjoo1015@naver.com>
2022-09-22 14:48:31 +0900

    Add parameter number check

--- 03/multi-table.sh
+++ 03/multi-table.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
-for i in $(seq 1 $1)
-do
-	line=""
-	for j in $(seq 1 $2)
+if [ $# -eq 2 ]
+then
+	if [ $1 -le 0 ] || [ $2 -le 0 ]
+	then
+		echo "Input must be greater than 0"
+		exit 0
+	fi
+	for i in $(seq 1 $1)
 	do
-		mul=`expr $i \* $j`
-		line+="$i*$j=$mul\t"
+		line=""
+		for j in $(seq 1 $2)
+		do
+			mul=`expr $i \* $j`
+			line+="$i*$j=$mul\t"
+		done
+		echo -e $line
 	done
-	echo -e $line
-done 
+else
+	echo "Invalid input"
+	exit 0
+fi
