d45a7bdf45a9332f0dd0ee0ba3ecf9458bbfa604
robinjoo1015 <robinjoo1015@naver.com>
2022-10-28 04:33:43 +0900

    Add simple kernel module

---
+++ 06/hello_module.c
@@ -0,0 +1,19 @@
+#--------- hello_module.c ---------#
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+int __init hello_module_init(void)
+{
+	printk("Hello Module!\n");
+	return 0;
+}
+
+void __exit hello_module_cleanup(void)
+{
+	printk("Bye Module!\n");
+}
+
+module_init(hello_module_init);
+module_exit(hello_module_cleanup);
+MODULE_LICENSE("GPL");
